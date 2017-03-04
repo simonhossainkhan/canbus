c017f4539f45b068b032c55cf4882e83d1915ae1
Salman Hasib <shasib@cpp.edu>
2017-03-04 12:57:10 -0800

    update on automation files, adding bluetooth connection check

--- automationFiles/testrun.sh
+++ automationFiles/testrun.sh
@@ -5,7 +5,10 @@ echo "Starting script"
 echo "Getting WiFi SSID"
 WIFISSDin=$(iwgetid -r)
 echo $WIFISSDin
-wificonnected=1
+connected=1
+
+echo "Waiting for Bluetooth to boot..."
+sleep 1
 
 echo "Beginning bluetooth commands"
 bluetoothctl << EOF
@@ -17,12 +20,20 @@ EOF
 sleep 1
 echo "Bluetooth commands finished"
 
+echo "Check for bluetooth connection"
+if hcitool dev | grep '00:1D:A5:00:04:34';then
+  echo "Bluetooth OBDII Reader connected"
+else
+  echo "Bluetooth not connected. Exiting."
+  exit 0
+fi
+
 echo "Starting RFCOMM"
-/home/pi/Documents/rfcomm_connect.sh &
+rfcomm_connect.sh &
 RFID=$!
 echo "RFCOMM PID"
 echo $RFID
-sleep 2
+sleep 1
 echo "RFCOMM connected"
 
 echo "Begin recording"
@@ -32,30 +43,30 @@ echo "Recording PID"
 echo $RID
 
 echo "Beginning Run"
-while [ connected==1 ]
+while [ $connected==1 ]
 do
   echo "Connected"
   TESTCON=$(iwgetid -r)
   if [[ "$TESTCON" == "$WIFISSIDin" ]];then
-    connected=1
+    $connected=1
   else
-    connected=0
+    $connected=0
   fi
 done
-while [ connected==0 ]
+while [ $connected==0 ]
 do
   echo "Running"
   if [[ "$(iwgetid -r)" == "" ]];then
-    connected=0
+    $connected=0
   elif [[ "$(iwgetid -r)" == "$WIFISSIDin" ]];then
-    connected=1
+    $connected=1
   fi
 done
 echo "Ending Run"
 
 echo "Releasing RFCOMM"
-/home/pi/Documents/rfcomm_disconnect.sh &
-sleep 5
+rfcomm_disconnect.sh &
+sleep 1
 echo "Released RFCOMM"
 
 
@@ -71,10 +82,11 @@ log=$(ls /home/pi/pyobd-pi/log)
 echo $log
 
 echo "Uploading file..."
-python /home/pi/Documents/upload_log.py $log
+python upload_log.py $log
 
 echo "Deleting all log files"
 rm /home/pi/pyobd-pi/log/*
 echo "Deleted files"
 
 echo "End of script"
+exit 0
